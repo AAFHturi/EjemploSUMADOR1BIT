cc12b4b8cd593852de08106af10e08d3e29e5f9f
AAFHturi <alberto.fh@matamoros.tecnm.mx>
2024-08-15 18:59:21 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,27 +1,15 @@
-/*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
- */
-
-`default_nettype none
-
-module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
-);
-
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
-
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
-
+module SUMADOR(
+input A,
+  input B,
+  input C_IN,
+  output SUMA, C_OUT );
+  
+  wire s1, c1, s2;
+  // instance
+  C_XOR U1(.OUT(s1),    .A(A),  .B(B)); // es la compuerta 1 
+  C_AND U2(.OUT(c1),    .A(A),  .B(B)); // la sintaxis es salida entrada entrada
+  C_XOR U3(.OUT(SUMA),  .A(s1), .B(C_IN));
+  C_AND U4(.OUT(s2),    .A(s1), .B(C_IN));
+  C_XOR U5(.OUT(C_OUT), .A(s2), .B(c1));
+    
 endmodule
