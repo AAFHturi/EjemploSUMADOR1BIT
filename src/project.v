0c3aab6df6127fa31859b6c774cd59768c671a9b
AAFHturi <alberto.fh@matamoros.tecnm.mx>
2024-08-15 20:20:41 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -28,5 +28,5 @@ module tt_um_EjemploSUMADOR1BIT (
  assign uio_oe  [7:0] = 8'b0;
  
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+ wire _unused = &{ena, clk, rst_n,ui_in[7:3],uio_in[7:0], 1'b0};
 endmodule
